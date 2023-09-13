1ef6fa6d00d2053be3c4d45e3213114e05981df2
Robin Söderholm <robin_soderholm1@hotmail.com>
2023-09-13 15:06:17 +0200

    test av att ladda upp

---
+++ öv 2.py
@@ -0,0 +1,8 @@
+strang1 = input ("Beskriv ditt intresse: ")
+strang2 = input ("Beskriv nån annans intresse: ")
+
+if len(strang1) == len(strang2):
+    print("Lika långa!")
+
+if strang1 == strang2:
+    print("Samma!")
