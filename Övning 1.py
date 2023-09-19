c92cef740c9c5e93dfe6f22756b9dde7a95c8ca8
Robin Söderholm <robin_soderholm1@hotmail.com>
2023-09-19 13:15:52 +0200

    test 2

---
+++ Övning 1.py
@@ -0,0 +1,33 @@
+volym = int(input ("Enter a volume in liters:  "))
+print("\n\n")
+
+
+print("Do you want to convert the volume to:\n 1. Milliliters \n 2. Centiliters \n 3. Deciliters \n 4. Cubic decimeters \n 5. Cubic meters \n ")
+
+enhet = int(input("Svar: "))
+
+if enhet == 1:
+    print (volym*1000)
+if enhet == 2:
+    print (volym*100)
+if enhet == 3:
+    print (volym*10)
+if enhet == 4:
+    print (volym*10*10)
+if enhet == 5:
+    print (volym*0.1*0.1)
+    
+if enhet != 1:
+    print("Plz choose between 1-5")
+    
+if enhet != 2:
+    print("Plz choose between 1-5")
+    
+if enhet != 3:
+    print("Plz choose between 1-5")
+    
+if enhet != 4:
+    print("Plz choose between 1-5")
+    
+if enhet != 5:
+    print("Plz choose between 1-5")    
